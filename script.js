b122acf878a2d5e03c614def50220d37eac1ffb2
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-06-15 13:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.replace(/\s+/g, "");
+	str = str.toLowerCase();
 
+	let checker = function (str) {
+		let s = 0; 
+		let e = str.length-1;
+		
+		while(s < e) {
+			
+			if(str.charAt(s) != str.charAt(e)){
+				return false;
+			}
+			s++;
+			e--;
+		}
+		return true;
+	}
+	return checker(str);
 }
 module.exports = palindrome
